2d0f063a9c1514a8e6f8af182492d589ec282f87
Peyton Clark <peytonclark@Peytons-MacBook-Pro-2.local>
2023-05-15 11:20:14 -0400

    Added comments to script

--- init_defang.py
+++ init_defang.py
@@ -1,20 +1,31 @@
-import re 
+#Imports Regular expression lib used for pattern matching 
+import re
+
+#Funtion defangs IPs using built-in python lib called replace
 
 def defang (ip_address):
 
     return ip_address.replace( '.','[.]')
 
+#Created inital input file and output file for defanged IPs 
+
 ip_defang = open('new_ips.txt', 'r')
 ip_defanged = open('defanged_ips.txt', 'w')
+
+# Loops through text file using built-in python lib called findall 
 
 for line in ip_defang:
     ip_addresses = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', line)
 
+# Takes defanged IPs from findall and creates a line variable 
+
     for ip_address in ip_addresses: 
         defanged_ip = defang(ip_address)    
         line = line.replace(ip_address, defanged_ip)
-        
+        # Writes defanged IPs to a new file 
         ip_defanged.write(line)
+
+#Closes all open files 
 
 ip_defang.close()
 ip_defanged.close() 
